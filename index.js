8d5d3f064d2df484e78db2c42608f099213980b5
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-24 19:47:58 +0300

    feat: add generation task with styles

--- index.js
+++ index.js
@@ -2,17 +2,40 @@ const appendBtn = document.querySelector('.add-button');
 const taskInput = document.getElementById('task-description');
 const tasksWrapper = document.querySelector('.tasks-wrapper');
 
-let tasks = [];
+let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
 function Task(description) {
     this.description = description;
     this.completed = false;
-}
+};
+
+const createTemplate = (task, index) => {
+    return `
+        <div class="task-item ${task.completed ? 'checked' : ''}">
+            <div class="description">${task.description}</div>
+            <div class="buttons">
+                <input class="finish-btn" type="checkbox" ${task.completed ? 'checked' : ''}>
+                <button class="delete-btn">Delete</button>
+            </div>
+        </div>
+    `;
+};
+
+const fillHtmlList = () => {
+    tasksWrapper.innerHTML = "";
+    if (tasks.length > 0) {
+        tasks.forEach(item, index => {
+            tasksWrapper.innerHTML += createTemplate(item, index);
+        });
+    }
+};
+
+fillHtmlList();
 
 const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+};
 
 appendBtn.addEventListener('click', () => {
     tasks.push(new Task(taskInput.value));
